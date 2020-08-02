4c2c58d2faef1f40c50c7c9cd51ea94cde12ab96
yiqing <yiqing_95@qq.com>
2020-08-02 12:46:44 +0800

    命令行程序常规套路

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,3 +1,32 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options{
+    #[structopt(default_value="Meow!")]
+    /// What does the cat say?
+    message: String , // [1]
+
+    #[structopt(short = "d" , long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+    let message = options.message;
+
+    let eye = if options.dead { "x" } else { "o" }; // [1]
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog");
+    }else{
+        println!("{}", message);
+    }
+
+
+    println!(" \\");
+    println!("ꂠ\\");
+    println!("ꂠꂠꀀ/\\_/\\");
+    println!("ꂠꂠ( {eye} {eye} )", eye=eye); // [2]
+    println!("ꂠꂠ=( I )=");
 }
---
+++ my_blogs/cli_apps.md
@@ -0,0 +1,25 @@
+##  命令行应用开发知识点
+
+### 流重定向
+
+redirecting the STDOUT and STDERR streams to
+separate files:
+> cargo run "woof" 1> stdout.txt 2> stderr.txt
+
+
+
+### arguments 应用参数
+
+- Binary Flags (aka Switches) 
+    binary flags, also known as toggles
+    or switches.
+    
+    long and short version of the flag 
+    flag 有长和短形式
+    
+    When a flag has the bool type, its values are determined by the
+    presence of it. If the flag is not present, it will be set to false and vice versa.
+
+- options
+    There are other types of arguments called options, which can take a
+    value (e.g., --value myvalue ),
