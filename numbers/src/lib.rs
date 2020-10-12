310509e77d21de15a5307a769028471e6d5ffb63
yiqing <yiqing_95@qq.com>
2020-10-13 06:34:10 +0800

    使用vec 向量版本重写打印功能

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -2,7 +2,7 @@ pub fn say_hello(){
     println!("hello, world!") ;
 }
 
-pub fn print(){
+pub fn print_0(){
     let numbers = [1,2,3,4,5] ;
     // Rust abstracts the idea of
     // iteration into yet another trait, this one called Iterator. We have to call iter here
@@ -17,4 +17,14 @@ pub fn print(){
 
     let numbers = [1u8 , 2,3,4,5] ;
     let numbers: [u8; 5] = [1,2,3,4,5] ;
+}
+
+pub fn print(){
+    let numbers = vec![
+    1,2,3,4,5] ;
+
+    for n in numbers{
+        println!("{}",n);
+    }
+
 }
