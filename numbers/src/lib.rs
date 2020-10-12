59057d8b122b093383ff378335784f1d3ef6211d
yiqing <yiqing_95@qq.com>
2020-10-13 06:46:47 +0800

    函数参数

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -1,30 +1,41 @@
-pub fn say_hello(){
-    println!("hello, world!") ;
-}
-
-pub fn print_0(){
-    let numbers = [1,2,3,4,5] ;
-    // Rust abstracts the idea of
-    // iteration into yet another trait, this one called Iterator. We have to call iter here
-    // to turn an array into an Iterator because arrays do not automatically coerce into
-    // into an Iterator.
-    for n in numbers.iter(){
-        println!("{}", n) ;
+mod _1 {
+    pub fn say_hello() {
+        println!("hello, world!");
     }
 
-    // trick: 通过模式匹配 故意制造错误 让编译器告诉我们类型
+    pub fn print_0() {
+        let numbers = [1, 2, 3, 4, 5];
+// Rust abstracts the idea of
+// iteration into yet another trait, this one called Iterator. We have to call iter here
+// to turn an array into an Iterator because arrays do not automatically coerce into
+// into an Iterator.
+        for n in numbers.iter() {
+            println!("{}", n);
+        }
+
+// trick: 通过模式匹配 故意制造错误 让编译器告诉我们类型
 //    let () = numbers ;
 
-    let numbers = [1u8 , 2,3,4,5] ;
-    let numbers: [u8; 5] = [1,2,3,4,5] ;
+        let numbers = [1u8, 2, 3, 4, 5];
+        let numbers: [u8; 5] = [1, 2, 3, 4, 5];
+    }
+
+    pub fn print() {
+        let numbers = vec![
+            1, 2, 3, 4, 5];
+
+        for n in numbers {
+            println!("{}", n);
+        }
+    }
 }
 
 pub fn print(){
-    let numbers = vec![
-    1,2,3,4,5] ;
-
-    for n in numbers{
-        println!("{}",n);
+    let numbers = [1,2,3,4,5] ;
+    output_sequence(numbers) ;
+}
+fn output_sequence(numbers: [u8; 5]) {
+    for n in numbers.iter(){
+        println!("{}",n) ;
     }
-
 }
