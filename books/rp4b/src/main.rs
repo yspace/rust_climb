900d4bcf9686da444344accc6e6ad170e1f0b515
yiqing <61391362@qq.com>
2021-05-30 19:42:32 +0800

    finish the guessing game

--- books/rp4b/src/main.rs
+++ books/rp4b/src/main.rs
@@ -1,16 +1,41 @@
+extern crate rand;
 use std::io;
+use rand::Rng ;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("please enter your hunch.");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    // println!("The secret number is: {}",secret_number) ;
 
-    let mut hunch = String::new() ;
+    loop {
+        println!("please enter your hunch.");
 
-    io::stdin().read_line(&mut hunch)
-    .ok()
-    .expect("Failed to read line") ;
-
-    println!("Your hunch was: {}", hunch) ;
-
+        let mut hunch = String::new() ;
+    
+        io::stdin().read_line(&mut hunch)
+        .ok()
+        .expect("Failed to read line") ;
+    
+        let hunch: u32 = match hunch.trim().parse() {
+            Ok(num) => num ,
+            Err(_) => continue ,
+        } ;
+        // .ok()
+        // .expect("Please enter a number!") ;
+    
+        println!("Your hunch was: {}", hunch) ;
+    
+        match hunch.cmp(&secret_number) {
+            Ordering::Less => println!("Very small"),
+            Ordering::Greater => println!("Very big!"),
+            Ordering::Equal => {
+                println!("You won!");
+                break ;
+            }
+        }
+    
+    }
+    
 }
