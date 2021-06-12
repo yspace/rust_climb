3ad6c2d2fae2d013b6ca080d5748aca86ad96912
yiqing <61391362@qq.com>
2021-06-12 23:16:49 +0800

    save function somewhere

--- hardway/src/funcs/mod.rs
+++ hardway/src/funcs/mod.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashMap;
+
 pub fn main() {
 
     // 在rust中，函数是一等公民（可以储存在变量/数据结构中，可以作为参数传入函数，
@@ -6,11 +8,35 @@ pub fn main() {
     say_what("xiaoli", hi) ;
     say_what("xiaoMing", hello) ;
 
+    // 存储在数据结构中
     fn foo() {println!("call foo!"); }
     fn bar() {println!("call bar!"); }
-    let kn = KnowledgeNode::new("foo", foo) ;
+    let mut kn = KnowledgeNode::new("foo", foo) ;
     println!("kn: {:?}", kn) ;
-    
+    kn.func = bar ;
+    println!("kn: {:?}", kn) ;
+    kn.func = foo ;
+
+    let mut fm = HashMap::new() ;
+    fm.insert("foo", kn) ;
+    let kn2 = KnowledgeNode::new("bar",bar)
+    .set_description(Some(String::from("hi this is desc"))) ;
+    fm.insert("bar", kn2) ;
+    for  name in fm.keys() {
+        println!("\n===== call {} ==========", name) ;
+        let node = fm.get(name).unwrap() ;
+        println!("name: {} ", node.name) ;
+        
+        if let Some(desc) = &node.description {
+            println!("description:{} ", desc) ;    
+        };
+        
+        let f = &node.func ;
+        f() ;
+
+        println!("===== end call {}/=== \n ", name) ;
+    }
+
 }
 
 
