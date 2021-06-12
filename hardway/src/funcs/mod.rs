80c2bd588315dc42bdd7fda3aa31affcd5034e55
yiqing <61391362@qq.com>
2021-06-12 23:36:22 +0800

    learn function arguments

--- hardway/src/funcs/mod.rs
+++ hardway/src/funcs/mod.rs
@@ -3,6 +3,14 @@ use std::collections::HashMap;
 
 pub fn main() {
 
+    func_params::run() ;
+
+}
+
+mod func_params{
+    use super::* ;
+
+    pub fn run() {
     // 在rust中，函数是一等公民（可以储存在变量/数据结构中，可以作为参数传入函数，
     // 可以作为返回值），所以rust的函数参数不仅可以是一般的类型，也可以是函数
     say_what("xiaoli", hi) ;
@@ -31,27 +39,46 @@ pub fn main() {
             println!("description:{} ", desc) ;    
         };
         
-        let f = &node.func ;
+        let f = &node.func ; // why can't directly call it: &node.func() 
         f() ;
 
         println!("===== end call {}/=== \n ", name) ;
     }
 
+    pattern_match() ;
+    }
+
+
+    fn hi(name: &str) {
+        println!("hi {}", name) ;
+    }
+    fn hello(name: &str) {
+        println!("hello {}", name) ;
+    }
+    
+    fn say_what(name: &str, func: fn(&str)) {
+        func(name) ;
+    }
+
+    fn pattern_match(){
+        fn print_id((name, age): (&str, i32)) {
+            println!(" i am {} and i am {} years old .", name, age);
+          }
+          
+          // 参数的模式匹配跟let语句的匹配一样，也可以使用下划线来表示丢弃一个值
+          fn print_age((_, age): (&str, i32)) {
+            println!("My age is  {} ;", age);
+          }
+
+          let qing = ("yiqing", 18) ; // ^-^ I am always 18 !
+          print_id(qing) ;
+          print_age(qing) ;
+    }
 }
 
 
 
-fn hi(name: &str) {
-    println!("hi {}", name) ;
-}
-fn hello(name: &str) {
-    println!("hello {}", name) ;
-}
-
-fn say_what(name: &str, func: fn(&str)) {
-    func(name) ;
-}
-
+/// Language points; Knowledge Points; Knowledge;
 #[derive(Debug)]
 struct KnowledgeNode{
     name: String ,
