1eec8a7c6775d102e2e4fc3891a0c2cfb0274e1c
yiqing <61391362@qq.com>
2021-06-14 18:15:03 +0800

    lean control flow

--- books/intro2p/src/main.rs
+++ books/intro2p/src/main.rs
@@ -5,5 +5,6 @@ fn main() {
 
     // chapters::types_and_values::main();
     // chapters::variables_and_boxes::main();
-    chapters::functions::main();
+    // chapters::functions::main();
+    chapters::control_flow::main();
 }
