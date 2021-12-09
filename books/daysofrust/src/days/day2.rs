5108cdabc4dfbd630a16477ee822f69d14302f6f
yiqing <yiqing_95@qq.com>
2021-12-09 11:05:02 +0800

    day2

---
+++ books/daysofrust/src/days/day2.rs
@@ -0,0 +1,34 @@
+use primal::Sieve ;
+
+pub fn main(){
+  let sieve = Sieve::new(10000) ;
+  let sieve = Sieve::new(10000);
+  let suspect = 5273;
+  println!("{} is prime: {}", suspect, sieve.is_prime(suspect));
+  let not_a_prime = 1024;
+  println!("{} is prime: {}", not_a_prime, sieve.is_prime(not_a_prime));
+
+
+  find_nth_prime(&sieve);
+
+  factorization(&sieve);
+}
+
+fn find_nth_prime(sieve: &Sieve){
+    let n = 1000;
+    match sieve.primes_from(0).nth(n - 1) {
+        Some(number) => println!("{}th prime is {}", n, number),
+        None => println!("I don't know anything about {}th prime.", n),
+    }
+}
+
+fn factorization(sieve: &Sieve){
+    println!("{:?}", sieve.factor(2610));
+}
+
+fn num_divisors(n: usize, primes: &Sieve) -> Option<usize> {
+    match primes.factor(n) {
+        Ok(factors) => Some(factors.into_iter().fold(1, |acc, (_, x)| acc * (x + 1))),
+        Err(_) => None,
+    }
+}
---
+++ books/daysofrust/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+mod days ;
+
+type ModuleMain = Box<dyn Fn()->()> ;
+
+fn main() {
+    
+    println!("Hello, world!");
+
+    let mut module_mains:HashMap<String, ModuleMain> = HashMap::new() ;
+
+    module_mains.insert(
+        "day2".to_string(),
+        Box::new(days::day2::main));
+    // days::day2::main() ; 
+
+    for (key, value) in &module_mains {
+        println!("call:{:?} value :{:?}", key, value());
+    }
+}
+
