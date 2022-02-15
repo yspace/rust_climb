535d0ecbee75e5ce708deb8aa977846d718f5414
yiqing <yiqing_95@qq.com>
2022-02-15 15:18:28 +0800

    start learning the svelte framework

---
+++ starters/hi_svelte/_dev_blogs/readme.md
@@ -0,0 +1,28 @@
+[svelte tailwind](https://scottspence.com/posts/how-to-set-up-svelte-with-tailwind)
+
+如果因为代理等原因报错
+查看配置
+~~~
+npm config ls
+
+~~~
+
+~~~
+解决办法：
+
+1、执行：
+
+npm config get proxy
+npm config get https-proxy
+如果返回值不为null，继续执行：
+（这一步很重要，一定要保证两个命令的返回值都为null,话说回来，应该出现这个错误这两个返回值有不为null的）
+npm config set proxy null
+npm config set https-proxy null
+2、执行：
+npm config set registry http://registry.cnpmjs.org/
+~~~
+
+也可以使用cnpm
+> npm install -g cnpm --registry=https://registry.npm.taobao.org 
+
+或者使用代理试试
---
+++ starters/hi_svelte/src/routes/index.svelte
@@ -0,0 +1,71 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+
+	<h1 class="text-4xl font-bold underline">    Hello world!  </h1>
+
+test the button 
+<a role="button" class="btn">Link</a> 
+<button type="submit" class="btn">Button</button> 
+<input type="button" value="Input" class="btn"> 
+<input type="submit" value="Submit" class="btn"> 
+<input type="reset" value="Reset" class="btn">
+
+
+
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
