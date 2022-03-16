6a8792629fb38fb02da8959da88b77df9957e7a1
yiqing <yiqing_95@qq.com>
2022-03-16 09:22:41 +0800

    start the rust cli  Getting the arguments

---
+++ books/rust_cli/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+    println!("Hello, world!");
+}
