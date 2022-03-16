e58341775c3066d0d3aed77b420dc28c716ed5da
yiqing <yiqing_95@qq.com>
2022-03-16 09:31:09 +0800

    manually parse the list of strings we get from the operating system

--- books/rust_cli/src/main.rs
+++ books/rust_cli/src/main.rs
@@ -1,5 +1,15 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf, // PathBuf is like a String but for file system paths that work cross-platform
+}
+
 fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
     println!("Hello, world!");
 }
