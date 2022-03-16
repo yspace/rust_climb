40beadd91b7166f3e947f5e0cde68547d924da48
yiqing <yiqing_95@qq.com>
2022-03-16 11:23:15 +0800

    error handle with context info

--- books/rust_cli/src/main.rs
+++ books/rust_cli/src/main.rs
@@ -17,14 +17,57 @@ fn main() {
 
     // println!("args: {:?}", args);
     // println!("args: {args:?}" );
-    let content =
-     std::fs::read_to_string(&args.path).expect("could not read file");
-     ; //optimize: use a BufReader instead of read_to_string()
+    let content = std::fs::read_to_string(&args.path).expect("could not read file"); //optimize: use a BufReader instead of read_to_string()
 
-     for line in content.lines() {
+    for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
-        } 
-     }
+        }
+    }
+}
 
+mod example {
+    fn main() -> Result<(), Box<dyn std::error::Error>> {
+        let result = std::fs::read_to_string("test.txt");
+        let content = match result {
+            Ok(content) => content,
+            Err(error) => {
+                return Err(error.into());
+            }
+        };
+        println!("file content: {}", content);
+        Ok(())
+    }
+}
+
+mod question_mark {
+    fn main() -> Result<(), Box<dyn std::error::Error>> {
+        let content = std::fs::read_to_string("test.txt")?;
+        println!("file content: {}", content);
+        Ok(())
+    }
+}
+
+mod providing_context {
+    #[derive(Debug)]
+    struct CustomError(String);
+
+    fn main() -> Result<(), CustomError> {
+        let path = "test.txt";
+        let content = std::fs::read_to_string(path)
+            .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
+        println!("file content: {}", content);
+        Ok(())
+    }
+    mod _anyhow {
+        use anyhow::{Context, Result};
+
+        fn main() -> Result<()> {
+            let path = "test.txt";
+            let content = std::fs::read_to_string(path)
+                .with_context(|| format!("could not read file `{}`", path))?;
+            println!("file content: {}", content);
+            Ok(())
+        }
+    }
 }
