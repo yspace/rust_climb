24464d770747594576d175a8729c45a53a0d2f50
yiqing <yiqing_95@qq.com>
2022-03-16 10:00:07 +0800

    Parsing CLI arguments with Clap

--- books/rust_cli/src/main.rs
+++ books/rust_cli/src/main.rs
@@ -1,15 +1,18 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
-    path: std::path::PathBuf, // PathBuf is like a String but for file system paths that work cross-platform
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
 
+
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("Hello, world!");
 }
