4cf8dab61ba6d15f10ee7342d3e28cc60a286903
yiqing <yiqing_95@qq.com>
2022-04-10 17:18:01 +0800

    refinery can't work

---
+++ learnings/hi_refinery/src/main.rs
@@ -0,0 +1,13 @@
+// use rusqlite::Connection;
+
+// mod embedded {
+//     use refinery::embed_migrations;
+//     embed_migrations!("tests/migrations");
+// }
+
+fn main() {
+    // let mut conn = Connection::open_in_memory().unwrap();
+    // embedded::migrations::runner().run(&mut conn).unwrap();
+
+        println!("can't work");
+}
