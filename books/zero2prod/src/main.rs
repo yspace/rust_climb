4efeea2c680d80423545ba063f0316f8ebc6b1d1
yiqing <yiqing_95@qq.com>
2022-04-21 16:15:31 +0800

    更改项目结构 为测试做准备

--- books/zero2prod/src/main.rs
+++ books/zero2prod/src/main.rs
@@ -1,30 +1,7 @@
-use actix_web::{
-    web, App, HttpRequest, HttpServer, 
-    Responder,HttpResponse
-    
-};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    // You can only use impl Trait if your function returns a single type;
-    //  if you want to return multiple, you need dynamic dispatch
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-  HttpResponse::Ok().finish()
-}
+use zero2prod::run ;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
    
-    HttpServer::new(|| {
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await 
+    run().await
 }
