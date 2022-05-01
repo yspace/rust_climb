ee9c79c00c5cd9193ca5e96d292904247f2ee0e0
yiqing <yiqing_95@qq.com>
2022-05-01 16:12:51 +0800

    测试环境变量

--- books/zero2prod/src/main.rs
+++ books/zero2prod/src/main.rs
@@ -5,8 +5,15 @@ use zero2prod::startup::run;
 
 use sqlx::{Connection, PgConnection};
 
+
+use dotenv::dotenv;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    dotenv::dotenv().ok();
+    println!("{}", std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"));
+
     // let file = file!();
     // let pb = std::path::Path::new(&file)
     // .parent().unwrap()
@@ -24,6 +31,8 @@ async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration."); // We have removed the hard-coded `8000` - it's now coming from our settings!
     println!("{:#?}", configuration);
+
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection)?.await
