cb79a5802aba42ba5eda0ce685733cb6b7b274c8
yiqing <yiqing_95@qq.com>
2022-05-12 09:30:52 +0800

    Moving the query extraction code to its own function

--- books/web_dev/src/listings/listing_4_27.rs
+++ books/web_dev/src/listings/listing_4_27.rs
@@ -63,6 +63,23 @@ struct Pagination {
     start: usize,
     end: usize,
 }
+fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
+    if params.contains_key("start") && params.contains_key("end") {
+        return Ok(Pagination {
+            start: params
+                .get("start")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(Error::ParseError)?,
+            end: params
+                .get("end")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(Error::ParseError)?,
+        });
+    }
+    Err(Error::MissingParameters)
+}
 
 pub async fn main() {
     let store = Store::new();
