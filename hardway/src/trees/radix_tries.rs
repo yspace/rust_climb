3f237387c56f75a38465973fb96922e212b9d8b2
yiqing <yiqing_95@qq.com>
2022-06-12 11:02:39 +0800

    radix_trie

--- hardway/src/trees/radix_tries.rs
+++ hardway/src/trees/radix_tries.rs
@@ -5,13 +5,31 @@ pub fn run() {
 
     let mut t:Trie<&str, fn()> = Trie::new();
     t.insert("hi", || {
-        println!("hi");
+        println!("fn: hi");
+    });
+    t.insert("hi_qing", || {
+        println!("fn: hi qing");
     });
 
     t.insert("hello", || {
-        println!("hello");
+        println!("fn: hello");
     });
 
     t.get("hi").unwrap()();
     t.get("hello").unwrap()();
+
+    println!("all keys:");
+    for k in t.keys() {
+        println!("{}", k);
+    }
+
+   let v =  t.subtrie("hi").unwrap();
+    println!("subtrie hi:");
+//    println!("{:?}", v);
+   for k in v.keys() {
+         println!("{}", k);
+   }
+   v.get("hi").unwrap().unwrap()();
+   v.get("hi_qing").unwrap().unwrap()();
+
 }
