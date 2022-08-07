6678a5637435fff1a8f8e6c7479a9eb8589c8bd7
yiqing <yiqing_95@qq.com>
2022-08-07 11:20:20 +0800

    学习hecto博客的内容

--- my_blogs/refs.md
+++ my_blogs/refs.md
@@ -3,3 +3,7 @@ https://lib.rs/#home-categories
 
 (一些有用的 crate )[https://learnku.com/docs/rust-cla-2020/some-useful-crites/10293]
 [一些好用的rust库 或者项目 类似awesome风格 进行的归类整理](http://fancy.rs/)
+
+### 一些rust博客
+
+https://hermanradtke.com/tags/rustlang.html
---
+++ projects/hecto/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::{self,stdout, Read};
+use termion::raw::IntoRawMode ;
+
+fn main() {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        print!("{}", c);
+
+        if c == 'q'{
+            break;
+        }
+    }
+
+
+}
+
