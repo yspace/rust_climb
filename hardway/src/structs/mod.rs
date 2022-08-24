1dfaf725a529221b63dde163dc4cd2f5d3afe318
yiqing <yiqing_95@qq.com>
2022-08-24 12:33:03 +0800

    结构体 封装数据和行为

--- hardway/src/structs/mod.rs
+++ hardway/src/structs/mod.rs
@@ -1,9 +1,9 @@
+mod chat;
 mod tuple_structs;
-mod chat ;
 
 pub fn run() {
-     // 最近学习的模块入口
-     return chat::main() ;
+    // 最近学习的模块入口
+    return chat::main();
 
     {
         let bg = Color {
@@ -15,16 +15,15 @@ pub fn run() {
         println!("{},{},{}", bg.red, bg.green, bg.blue);
     }
     {
-        let mut bg = Color{
+        let mut bg = Color {
             red: 255,
             green: 70,
             blue: 15,
         };
-        
+
         bg.blue = 45;
 
-        println!("{},{},{}", bg.red, bg.green, bg.blue); 
-
+        println!("{},{},{}", bg.red, bg.green, bg.blue);
     }
 
     tuple_structs::run();
@@ -34,4 +33,28 @@ struct Color {
     red: u8,
     green: u8,
     blue: u8,
+}
+
+struct Door {
+    width: u32,
+    height: u32,
+    is_open: bool,
+}
+
+impl Door {
+    pub fn new(width: u32, height: u32, is_open: bool) -> Door {
+        Door {
+            width,
+            height,
+            is_open,
+        }
+    }
+
+    pub fn open(&mut self) {
+        self.is_open = true;
+    }
+
+    pub fn close(&mut self) {
+        self.is_open = false;
+    }
 }
