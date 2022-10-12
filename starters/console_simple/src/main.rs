416c66b0cff4575439bfd6c3659e607adf5891ca
yiqing <yiqing_95@qq.com>
2022-10-12 08:03:55 +0800

    早期 c｜unix 风格的main程序

--- starters/console_simple/src/main.rs
+++ starters/console_simple/src/main.rs
@@ -1,7 +1,6 @@
 //! An example of simple console app in rust.
 use std::env;
 
-
 fn main() {
     let from = if env::args_os().count() == 2 {
         env::args_os().nth(1).unwrap()
@@ -10,10 +9,10 @@ fn main() {
         std::process::exit(1);
     };
 
-     println!("your input : {:?}", from);
+    println!("your input : {:?}", from);
 }
 
-fn arg1(){
+fn arg1() {
     let from = if env::args_os().count() == 2 {
         env::args_os().nth(1).unwrap()
     } else {
@@ -22,7 +21,7 @@ fn arg1(){
     };
 }
 
-fn arg2(){
+fn arg2() {
     let (from, into) = if env::args_os().count() == 3 {
         (
             env::args_os().nth(1).unwrap(),
@@ -32,4 +31,62 @@ fn arg2(){
         println!("Please enter a from and into path.");
         std::process::exit(1);
     };
+}
+
+// from https://github.com/zip-rs/zip/blob/master/examples/file_info.rs
+mod example2 {
+    use std::fs;
+    use std::io::BufReader;
+
+    fn main() {
+        std::process::exit(real_main());
+    }
+
+    fn real_main() -> i32 {
+        let args: Vec<_> = std::env::args().collect();
+        if args.len() < 2 {
+            println!("Usage: {} <filename>", args[0]);
+            return 1;
+        }
+        let fname = std::path::Path::new(&*args[1]);
+        let file = fs::File::open(&fname).unwrap();
+        let reader = BufReader::new(file);
+
+        // let mut archive = zip::ZipArchive::new(reader).unwrap();
+
+        // for i in 0..archive.len() {
+        //     let file = archive.by_index(i).unwrap();
+        //     let outpath = match file.enclosed_name() {
+        //         Some(path) => path,
+        //         None => {
+        //             println!("Entry {} has a suspicious path", file.name());
+        //             continue;
+        //         }
+        //     };
+
+        //     {
+        //         let comment = file.comment();
+        //         if !comment.is_empty() {
+        //             println!("Entry {} comment: {}", i, comment);
+        //         }
+        //     }
+
+        //     if (*file.name()).ends_with('/') {
+        //         println!(
+        //             "Entry {} is a directory with name \"{}\"",
+        //             i,
+        //             outpath.display()
+        //         );
+        //     } else {
+        //         println!(
+        //             "Entry {} is a file with name \"{}\" ({} bytes)",
+        //             i,
+        //             outpath.display(),
+        //             file.size()
+        //         );
+        //     }
+        // }
+
+        0
+    }
 }
