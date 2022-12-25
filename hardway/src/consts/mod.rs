f1a43c36a665c785b9c2d8b73fb2bd1d88b9dea5
yiqing <yiqing_95@qq.com>
2022-12-25 10:39:55 +0800

    学习常量

--- hardway/src/consts/mod.rs
+++ hardway/src/consts/mod.rs
@@ -1,7 +1,20 @@
 const PI: f64 = 3.14159265 ;
+// 对于 const ，常量贯穿于整个程序的生命周期。更具体的，Rust 中的常量并没有固定的内存地址。
+// 这是因为实际上它们会被 内联 到用到它们的地方。 为此对同一常量的引用并不能保证引用到相同的内存地址。
+
+
+
+// 参考 ://www.codercto.com/a/89622.html
 
 pub fn main(){
     println!("PI is {}", PI);
 
     println!("f64: from {} to {} ",f64::MIN, f64::MAX);
+
+    // let p1 = PI  ;
+    // let p2 = PI ;
+    // println!("p1: {:p}", &p1);
+    // println!("p2: {:p}", &p2);
+
+
 }
