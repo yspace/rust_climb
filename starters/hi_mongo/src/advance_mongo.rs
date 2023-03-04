960bd270e4a867a6adca4be58eb1c0a3310df677
yiqing <yiqing_95@qq.com>
2023-03-04 22:42:33 +0800

    mongo 聚合操作 投影

--- starters/hi_mongo/src/advance_mongo.rs
+++ starters/hi_mongo/src/advance_mongo.rs
@@ -2,7 +2,7 @@ use super::*;
 
 pub async  fn run(db: mongodb::Database) -> Result<()> {
 
-    // init_collection(db.clone()).await?;
+    init_collection(db.clone()).await?;
 
     // and_ops(db.clone()).await?;
     // or_ops(db.clone()).await?;
@@ -14,9 +14,17 @@ async fn init_collection(db: mongodb::Database) -> Result<()> {
     let collection = db.collection::<Document>("adv_person");
 
     let docs = vec![
-        doc! { "title": "1984", "name": "George Orwell" ,"age":16, "gender":1},
-        doc! { "title": "Animal Farm", "name": "George Orwell" , "age": 32 , "gender":0},
-        doc! { "title": "The Great Gatsby", "name": "F. Scott Fitzgerald" , "age":20 , "gender":1},
+        doc! { "title": "1984", "name": "George Orwell" ,"age":16, "gender":1 , "tags":["富有","白"], "profile":{"rich":1}},
+        doc! { "title": "1984", "name": "George Orwell" ,"age":16, "gender":1 , "tags":["富有","白"], "profile":{"rich":1}},
+        doc! { "title": "1984", "name": "George Orwell" ,"age":16, "gender":1 , "tags":["富有","白"], "profile":{"rich":1}},
+        doc! { "title": "1984", "name": "George Orwell" ,"age":16, "gender":1 , "tags":["富有","白"], "profile":{"rich":1}},
+       
+        doc! { "title": "Animal Farm", "name": "George Orwell" , "age": 32 , "gender":0, "tags":["富有","矮小","猥琐"], "profile":{"rich":1}},
+        doc! { "title": "Animal Farm", "name": "George Orwell" , "age": 32 , "gender":0, "tags":["富有","矮小","猥琐"], "profile":{"rich":1}},
+        doc! { "title": "Animal Farm", "name": "George Orwell" , "age": 32 , "gender":0, "tags":["富有","矮小","猥琐"], "profile":{"rich":1}},
+       
+        doc! { "title": "The Great Gatsby", "name": "F. Scott Fitzgerald" , "age":20 , "gender":1, "tags":["穷","白","美"], "profile":{"rich":0}},
+        doc! { "title": "The Great Gatsby", "name": "F. Scott Fitzgerald" , "age":20 , "gender":1, "tags":["穷","白","美"], "profile":{"rich":0}},
     ];
 
     // Insert some documents into the "mydb.books" collection.
@@ -85,7 +93,14 @@ async fn aggregate_ops(db: mongodb::Database) -> Result<()> {
     let pipeline = vec![ 
         // match 阶段 跟find 接受的参数要求是一样的 
         doc!{"$match":{"age":{"$gt":10}, "gender":0 }},
-        doc!{"$project":{"_id":0 }},
+        // project 是投影的意思 可以缩小范围 也可以放大 就像你用手电筒照墙壁一样 影子大小可以通过调焦缩放
+        // doc!{"$project":{"_id":0 , "title":1, "name":1, "gender":1 ,"added_key":"some_value","added_key2":"$name"}},
+        // 修改已有字段 name的输出; 最后一个字段也是用此技巧 拉平嵌套文档中的某字段
+        doc!{"$project":{"_id":0 , "title":1, "name":"yaaaa!", "gender":1 ,"added_key":"some_value",
+        "added_key2":"$name","profile_rich":"$profile.rich"
+        ,"some_name_contain_doller_sign":{"$literal":"$normal_string"}
+    }},
+        // doc!{"$project":{"new_k":"any_value" }},
          ];
     let options = None;
     let result = collection.aggregate(pipeline, options).await?;
