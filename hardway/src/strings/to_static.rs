91de1b6a301999c9469434e2fcdec458dd8e891d
yiqing <yiqing_95@qq.com>
2023-04-03 11:38:13 +0800

    string 转静态str

---
+++ hardway/src/strings/to_static.rs
@@ -0,0 +1,11 @@
+// https://stackoverflow.com/questions/23975391/how-to-convert-a-string-into-a-static-str
+
+fn string_to_static_str(s: String) -> &'static str {
+    Box::leak(s.into_boxed_str())
+}
+
+fn main() {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    let s: &'static str = string_to_static_str(s);
+}
