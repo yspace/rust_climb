23e914d762d5b883b291fbb430e7edd5ffbee18c
yiqing-95 <61391362@qq.com>
2023-12-05 11:11:01 +0800

    学习评分最高的那本书

--- hardway/src/funcs/basics.rs
+++ hardway/src/funcs/basics.rs
@@ -1,21 +1,22 @@
-fn gcd(mut n: u64, mut m: u64) -> u64 
-{ 
+fn gcd(mut n: u64, mut m: u64) -> u64
+{
     // There is also a debug_assert! macro, whose assertions are skipped when the program is compiled for speed.
     assert!(n != 0 && m != 0);
-     whilem!=0{
-    ifm<n{ lett=m;
-    m=n;
-    n=t; }
-    m=m%n; }
+    while m != 0
+    {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
     n
-
-
-    
 }
 
-fn return_expression()-> i32{
+fn return_expression() -> i32 {
     let var1 = {
-        println!("do something! "); 
+        println!("do something! ");
         "some returned var"
     };
 
